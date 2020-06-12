27a3b8c761a8fff6c9d1051385f1ef245cedaa43
zhouhong07 <zhouhong07@58.com>
2020-06-12 09:06:35 +0800

    mobile页面重新命名

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  * @Author: zhouhong07
  * @Date: 2020-05-08 10:37:09
  * @LastEditors: zhouhong07
- * @LastEditTime: 2020-06-05 17:58:42
+ * @LastEditTime: 2020-06-12 09:06:00
  */
 var express = require('express');
 var router = express.Router();
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
   var deviceAgent = req.headers["user-agent"].toLowerCase();
   var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
   if(agentID){
-    res.render('error', { title: 'Express' },(err, html) => {
+    res.render('mobile', { title: 'Express' },(err, html) => {
       res.set('Content-Type', 'text/html')
       res.send(html) ;
     });
